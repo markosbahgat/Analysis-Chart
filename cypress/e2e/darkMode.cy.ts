d098f226bc2a0665a2f1a6a51e22b749737e7538
markosbahgat <markos.bahgat@gmail.com>
2022-06-22 16:13:20 +0200

    Update Readme

--- cypress/e2e/darkMode.cy.ts
+++
@@ -1,8 +0,0 @@
-export const TestingDarkMode = () => {
-  it("DarkMode", () => {
-    cy.restoreLocalStorage();
-    cy.visit("localhost:3000/");
-    cy.get('button[id="headlessui-switch-:r1:"]').click();
-    cy.get("main[data-testid=main]").should("have.class", "bg-gray-400");
-  });
-};
--- cypress/e2e/signinForm.cy.ts
+++
@@ -1,8 +0,0 @@
-export const SignInForm = () => {
-  it("Test SignIn", () => {
-    cy.get('input[name="email"]').type("markos.a7a@gmail.com");
-    cy.get('input[name="password"]').type("dfsafdsfdsadsfsda");
-    cy.get("form").submit();
-    cy.saveLocalStorage();
-  });
-};
